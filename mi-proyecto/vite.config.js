dbe62396fc56ae38b9e014c30db468208ebf8dd7
matifagith <matiasfarinasola@gmail.com>
2026-09-15 23:45:39 -0300

    corrigiendo tp2

--- mi-proyecto/vite.config.js
+++ mi-proyecto/vite.config.js
@@ -1,7 +1,11 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ucasal_leng_4_tp2/', // Fundamental para que carguen los CSS y JS
+  build: {
+    outDir: '../docs', // Le dice a Vite que genere la compilación un nivel arriba, en /docs
+    emptyOutDir: true
+  }
 })
